ae20f788a3170656ac8406b96d07d58749847d31
Dean Krotzer <deankrotzer@Deans-MacBook-Pro.local>
2020-05-20 13:58:51 -0400

    --- Auto-Git Commit ---

--- src/components/pages/Home/PuppyCard.js
+++ src/components/pages/Home/PuppyCard.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import './puppycard.css';
-import { CardGroup, Card} from 'react-bootstrap';
-import {Button, Modal, Carousel, Tabs, Tab} from 'react-bootstrap';
+import { CardGroup, Card, CardDeck, Button} from 'react-bootstrap';
 import PuppyRec from './PuppyRec';
 import PuppyPackage from './PuppyPackage';
 import MeetParents from '../Home/Parents/MeetParents';
 const PuppyCard = ({status, name, gender, price}) => {
     
     return (
-        <Card className="cardContainer">
+        <CardDeck className="ModalStyle">
+        
+        <Card>
  
-    <Card.Img variant="top"  id="harley" />
     <Card.Body>
       <Card.Title>{name}
       </Card.Title>
-      <div className="puppytextcard">
+      
       <Card.Text>
         <div className="puppyDetails">
         {gender}
         
 
 </div>
-      </Card.Text></div>
+      </Card.Text>
       {status}
       {price}
       <Button> More Info</Button>
@@ -29,8 +29,9 @@ const PuppyCard = ({status, name, gender, price}) => {
     <Card.Footer>
       <small className="text-muted">Avalible to leave our home to yours by July</small>
     </Card.Footer>
-  </Card>
-
+    </Card>
+    
+</CardDeck>
 
     );
 }
